dfe302e3aacaf725164b741a0567d325207bdf69
Davi Dias <157860462+DevDaviDias@users.noreply.github.com>
2025-11-07 00:34:22 +0000

    primeira parte: estrutura inicial do progeto pronta

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+app.use(express.static("public"));
+io.on("connection",(socket) =>{
+    console.log("um usuario se conectou");
+});
